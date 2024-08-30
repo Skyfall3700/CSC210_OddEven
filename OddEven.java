7b70abdc2c6cf6a70df58167e3f445cdd3be6ce6
Matthew Nguyen <91217088+Skyfall3700@users.noreply.github.com>
2024-08-30 09:04:10 -0700

    Add files via upload

---
+++ OddEven.java
@@ -0,0 +1,10 @@
+package com.comgradescope.oddeven;
+
+public class OddEven {
+	public boolean isEven(int number) {
+		return (number % 2 == 0);
+	}
+	public boolean isOdd(int number) {
+		return (number %2 != 0);
+	}
+}
